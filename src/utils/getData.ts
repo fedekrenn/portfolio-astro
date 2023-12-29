0409882908c8ebf566d4b563c3c4ba3f7f241dd7
Federico Krenn <fedekrenn@gmail.com>
2023-12-29 12:35:49 -0300

    Update project images to webp format

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,20 +1,16 @@
-import { data } from './experience.json'
-import type { Project } from './types'
-
+import { data } from "./experience.json";
+import type { Project } from "./types";
 
 export const getExperience = () => {
-  return data.filter(item => item.client !== 'Proyecto personal')
-}
+  return data.filter((item) => item.client !== "Proyecto personal");
+};
 
 export const getProjects = () => {
-  return data.filter(item => item.client === 'Proyecto personal')
-}
+  return data.filter((item) => item.client === "Proyecto personal");
+};
 
 export const getById = (id: number): Project => {
-  const result = data.find(item => item.id === id)
-  if (!result) throw new Error('No existe el proyecto')
-  return result
-}
-
-
-
+  const result = data.find((item) => item.id === id);
+  if (!result) throw new Error("No existe el proyecto");
+  return result;
+};
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -11,4 +11,3 @@ export interface Project {
   repository: string;
   image: string;
 }
-
