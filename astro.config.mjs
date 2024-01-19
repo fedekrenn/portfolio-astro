26db960c04afbe22b413ec21596adf75f284117a
Federico Krenn <fedekrenn@gmail.com>
2024-01-19 12:18:36 -0300

    Add sitemap integration and update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  site: 'https://federico-krenn.vercel.app/',
+  integrations: [sitemap()]
+})
